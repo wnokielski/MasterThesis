bf883c3f87a65c07c6d28d1fcf39b2eb09ffa6fd
Wojciech Nokielski <wojciech.nokielski@gmail.com>
2024-03-22 21:56:22 +0100

    Spring Boot app + Docker files

---
+++ ClaimPortalJava/src/main/java/com/mt/claimportaljava/ClaimPortalJavaApplication.java
@@ -0,0 +1,13 @@
+package com.mt.claimportaljava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimPortalJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimPortalJavaApplication.class, args);
+	}
+
+}
---
+++ ClaimPortalJava/src/test/java/com/mt/claimportaljava/ClaimPortalJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mt.claimportaljava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimPortalJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
